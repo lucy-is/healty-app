7acc3bd97dce5ecbfee2e86a7ef592a284b10342
lucy <lucyk@o2pluss.com>
2022-07-29 18:47:51 +0900

    ADD: API 요청, CORS 해결

--- next.config.js
+++ next.config.js
@@ -1,8 +1,23 @@
-/**
- * @type {import('next').NextConfig}
- */
- const nextConfig = {
-    /* config options here */
-  }
+const nextConfig = {
+    webpack: (config, { webpack }) => {
+      const prod = process.env.NODE_ENV === 'production';
+      const newConfig = {
+        ...config,
+        mode: prod ? 'production' : 'development',
+      };
+      if (prod) {
+        newConfig.devtool = 'hidden-source-map';
+      }
+      return newConfig;
+    },
+    async rewrites() {
+      return [
+        {
+          source: '/api/:path*',
+          destination: `http://apis.data.go.kr/1471000/HtfsInfoService2/:path*`,
+        },
+      ];
+    },
+  };
   
-  module.exports = nextConfig
+  module.exports = nextConfig;
--- pages/Axios/AxiosApi.js
+++ pages/Axios/AxiosApi.js
@@ -6,22 +6,30 @@ function GetSupple() {
     const [loading, setLoading] = useState(null)
     const [error, setError] = useState(null)
     const [inputValue, setInput] = useState(null)
+    //F3qZpDpoCIhqF8zuV%2B1%2BE8UPiaRlfmy7fEZAJ2YTZ%2BcDwyauDgPE30CZW3cCs2kV2mTteWoCCN9QuIZfS7oZWA%3D%3D
+    //F3qZpDpoCIhqF8zuV+1+E8UPiaRlfmy7fEZAJ2YTZ+cDwyauDgPE30CZW3cCs2kV2mTteWoCCN9QuIZfS7oZWA==
 
     const fetchSupples = async () => {
         try {
           // 요청이 시작 할 때에는 error 와 supples를 초기화하고
           setError(null);
           setSupples(null);
+          setInput("")
           // loading 상태를 true 로 바꿉니다.
           setLoading(true);
           const params = {}
-          params.Entrps = "성풍양행"
-          params.ServiceKey = "F3qZpDpoCIhqF8zuV%2B1%2BE8UPiaRlfmy7fEZAJ2YTZ%2BcDwyauDgPE30CZW3cCs2kV2mTteWoCCN9QuIZfS7oZWA%3D%3D"
+          params.ServiceKey = "F3qZpDpoCIhqF8zuV+1+E8UPiaRlfmy7fEZAJ2YTZ+cDwyauDgPE30CZW3cCs2kV2mTteWoCCN9QuIZfS7oZWA=="
+          // params.Entrps = "성풍양행"
+          params.Prduct = inputValue
+          // params.Sttemnt_no = null
+          params.pageNo = null
+          params.numOfRows = null
+          params.type = "json"
           const response = await axios.get(
-            'http://apis.data.go.kr/1471000/HtfsInfoService2/getHtfsList',
-            params
+            '/api/getHtfsItem',
+            {params: params},
           );
-          setSupples(response.data); // 데이터는 response.data 안에 들어있습니다.
+          setSupples(response.data.header.resultMsg); // 데이터는 response.data 안에 들어있습니다.
         } catch (e) {
           setError(e);
         }
@@ -41,13 +49,14 @@ function GetSupple() {
     // 드디어 supples 성공적으로 받아와 진 상태입니다.
     return (
         <div>
-            <input type="text" id="name" name="name" value={inputValue} />
+          <input type="text" name="name" value={inputValue} onChange={e => setInput(e.target.value)}/>
             <ul>
-            {supples.map(item => (
+            {/* {supples.map(item => (
                 <li key={item.id}>
                 {item.username} ({item.name})
                 </li>
-            ))}
+            ))} */}
+            {supples}
             </ul>
             <button onClick={ fetchSupples }>다시 조회하기</button>
         </div>
